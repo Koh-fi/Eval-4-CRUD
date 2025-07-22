62f733a8d2bc03eaece900adeaa6f69834544f6a
Koh-fi <sebastian.poblete36@inacapmail.cl>
2025-07-22 15:14:34 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Link, Routes, Route, BrowserRouter } from 'react-router-dom';
 import { TodoList } from './componentes/TodoList';
+import { FetchPage } from './componentes/FetchPage';
+import { AxiosPage } from './componentes/AxiosPage';
 
 export function App() {
 
   return (
     <>
-      <div className='container m'>
-        <TodoList />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<TodoList />} />
+          <Route path='/fetch' element={<FetchPage />} />
+          <Route path='/axios' element={<AxiosPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
